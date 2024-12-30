a4e8db68442bde4e271af827a30561d269a621e6
Matěj Bucek <info@mbucek.cz>
2024-12-30 21:34:01 +0100

    RiscVOS/Syscalls: Renamed the Syscall impl. file names
    This better represents what each file implements.

--- src/Kernel/Syscalls/SyscallDebug.cpp
+++ src/Kernel/Syscalls/SyscallDebug.cpp
@@ -1,5 +1,16 @@
-//
-// Created by matejbucek on 30.12.24.
-//
+#include <Kernel/API/Syscall.h>
+#include <Kernel/Process/Process.h>
+#include <Kernel/Process/Userspace.h>
 
-#include "SyscallDebug.h"
+namespace Kernel {
+  ErrorOr<uintptr_t, SysError> Process::handle_dbgln(Userspace<char*> message) {
+    DebugConsole::print("Message from process: ");
+    auto error_or_message_in_kernel = message.get(m_page_table);
+    if(error_or_message_in_kernel.has_error()) {
+      return ErrorOr<uintptr_t, SysError>::create_error(SysError::Error);
+    }
+
+    DebugConsole::println(error_or_message_in_kernel.get_value());
+    return ErrorOr<uintptr_t, SysError>::create(0);
+  }
+}// namespace Kernel
--- src/Kernel/Syscalls/SyscallFile.cpp
+++ src/Kernel/Syscalls/SyscallFile.cpp
@@ -9,17 +9,21 @@ using Utils::ErrorOr;
 namespace Kernel {
   ErrorOr<uintptr_t, SysError> Process::handle_open(Userspace<char*> path, u64 flags) {
     auto& vfs = VirtualFileSystem::the();
+
+    // Translate the path from userspace to kernel space
     auto error_or_path_in_kernel = path.get(m_page_table);
     if(error_or_path_in_kernel.has_error()) {
-      return ErrorOr<uintptr_t, SysError>::create_error(SysError::Error);
+      return SysError::Error;
     }
 
+    // Copies the path from userspace to kernel space
     auto path_in_kernel = String(error_or_path_in_kernel.get_value());
 
     auto file_or_error = vfs.open(m_credentials, path_in_kernel, flags);
     if(file_or_error.has_error()) {
       DebugConsole::print("Failed to open file: ");
       DebugConsole::println(file_or_error.get_error().get_message().value());
+      return SysError::Error;
     }
 
     //auto open_or_error = ErrorOr<size_t>::create(0);
@@ -27,16 +31,17 @@ namespace Kernel {
     if(open_or_error.has_error()) {
       DebugConsole::print("Failed to open file: ");
       DebugConsole::println(open_or_error.get_error().get_message().value());
+      return SysError::Error;
     }
 
     DebugConsole::println("Opened file successfully");
-    return ErrorOr<uintptr_t, SysError>::create(open_or_error.get_value());
+    return open_or_error.get_value();
   }
 
   ErrorOr<uintptr_t, SysError> Process::handle_close(u64 fd) {
     auto descriptor = m_fd_table[fd];
     if(!descriptor) {
-      return ErrorOr<uintptr_t, SysError>::create_error(SysError::Error);
+      return SysError::Error;
     }
 
     m_fd_table.close(fd);
