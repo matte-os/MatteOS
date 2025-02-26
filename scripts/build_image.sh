14639676319c20c5d17657257e95bfed8af78a19
Matěj Bucek <info@mbucek.cz>
2025-02-26 09:54:20 +0100

    MatteOS/Build: Disk rebuild on run and no override warnings

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -3,6 +3,6 @@
 ninja
 cd build/dist/riscv
 riscv64-unknown-linux-gnu-objcopy -O binary -R .note -R .comment -S kernel.elf kernel.bin
-gzip -9 kernel.bin
+gzip -f -9 kernel.bin
 mkimage -A riscv -O linux -T kernel -C gzip -a 0x86000000 -e 0x86000000 -n "Matte OS" -d kernel.bin.gz uImage
 cd ../..
