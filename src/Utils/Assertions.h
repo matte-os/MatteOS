bab644925b67d1f64491914fea97ddb5e8ab1c31
Matěj Bucek <info@mbucek.cz>
2025-04-17 22:04:30 +0200

    MatteOS: Adding use_after_free assertion

--- src/Utils/Assertions.h
+++ src/Utils/Assertions.h
@@ -12,3 +12,9 @@
 
 #define runtime_assert(condition, message) runtime_assert_impl(condition, message, GET_CLASS_NAME, __LINE__)
 void runtime_assert_impl(bool condition, const char* message, const char* file, int line);
+
+#define use_after_free_assert(ptr) \
+  if(Kernel::KernelMemoryAllocator::the().is_in_freed_memory(reinterpret_cast<uintptr_t*>(ptr))) { \
+    __builtin_trap(); \
+    runtime_assert(false, "Using freed memory"); \
+  }
